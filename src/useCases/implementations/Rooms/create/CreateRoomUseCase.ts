b8430ed4dfe4b799b1a6207e41100be20cf264e1
Danilo Santana <danilosantana@MacBook-Pro-de-Danilo.local>
2021-08-15 11:57:46 -0300

    Check if room already exists on creation.

--- src/useCases/implementations/Rooms/create/CreateRoomUseCase.ts
+++ src/useCases/implementations/Rooms/create/CreateRoomUseCase.ts
@@ -6,7 +6,15 @@ import { Room } from "../../../../entities/Room";
 export class CreateRoomUseCase implements IUseCase<void> {
   constructor(private readonly repository: IRoomRepository) {}
 
-  public async execute(room: Room): Promise<void> {
-    await this.repository.save(room);
+  public async execute(data: Room): Promise<void> {
+    const alreadyExists: boolean = !!(await this.repository.findMany()).find(
+      (room) => room.name.toLowerCase() === data.name.toLowerCase(),
+    );
+
+    if (alreadyExists) {
+      throw new Error("Room already exists");
+    }
+
+    await this.repository.save(data);
   }
 }
