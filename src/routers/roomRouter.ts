38c621f071f87a2fd63fcfba85f7f3a34d5a3f5f
Raisess <raisechif@gmail.com>
2021-08-15 16:57:43 -0300

    Created find all rooms route.

--- src/routers/roomRouter.ts
+++ src/routers/roomRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
 import { createRoomController } from "../useCases/implementations/Rooms/create";
+import { findAllRoomsController } from "../useCases/implementations/Rooms/findAll";
 
 export const roomRouter: Router = Router();
 
 roomRouter.post("/rooms", (req, res) => createRoomController.handle(req, res));
+roomRouter.get("/rooms", (req, res) => findAllRoomsController.handle(req, res));
